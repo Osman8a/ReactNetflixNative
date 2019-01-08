8a8046dfeb47d3a73a66dd5efd270575ae7cd490
Osman Ochoa <ochoaosman@gmail.com>
2019-01-08 16:44:14 -0400

    Listado Horizontal de películas

--- components/src/List.js
+++ components/src/List.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import {
     Text,
     View,
-    StyleSheet,
-    FlatList
+    Image,
+    FlatList,
+    StyleSheet
 } from 'react-native';
-import movies from '../../config'
+import { movieOne, movieTwo } from '../../config'
 
 class List extends Component {
     constructor(props) {
@@ -15,21 +16,51 @@ class List extends Component {
 
     _renderItem(item) {
         return (
-            <Text>
-                {item.name}
-            </Text>
+            <Image
+                style={styles.imagen}
+                source={{ uri: item.image }}
+            />
         )
     }
     render() {
         return (
             <View style={{ flex: 1 }}>
-                <FlatList
-                    renderItem={({ item }) => this._renderItem(item)}
-                    data={movies}
-                />
+                <View>
+                    <Text style={styles.text}>My list</Text>
+                    <FlatList
+                        horizontal
+                        ItemSeparatorComponent={() => <View style={styles.separator} />}
+                        keyExtractor={(item) => item.id}
+                        renderItem={({ item }) => this._renderItem(item)}
+                        data={movieOne}
+                    />
+                </View>
+                <View>
+                    <Text style={styles.text}>Top pic for you</Text>
+                    <FlatList
+                        horizontal
+                        ItemSeparatorComponent={() => <View style={styles.separator} />}
+                        keyExtractor={(item) => item.id}
+                        renderItem={({ item }) => this._renderItem(item)}
+                        data={movieTwo.reverse()}
+                    />
+                </View>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    imagen: {
+        width: 120,
+        height: 180
+    },
+    separator: {
+        width: 5
+    },
+    text: {
+        color: 'white'
+    }
+});
 
 export default List;
